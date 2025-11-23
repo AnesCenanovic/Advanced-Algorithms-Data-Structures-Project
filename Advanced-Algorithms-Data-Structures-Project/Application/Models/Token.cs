08d6c9fc41dbc09fde86f8f783818751d762a293
Faris Crnčalo <115996601+fcrncalo1@users.noreply.github.com>
2025-11-23 16:48:41 +0100

    Format files

--- Advanced-Algorithms-Data-Structures-Project/Application/Models/Token.cs
+++ Advanced-Algorithms-Data-Structures-Project/Application/Models/Token.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Models
+﻿namespace Application.Models
 {
     public record Token(int Offset, int Length, char NextChar);
 }
